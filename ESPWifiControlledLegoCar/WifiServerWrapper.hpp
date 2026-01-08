161a6401f78dc394ef1c44580e157649313256b5
saucedeveloper <power16gd@gmail.com>
2026-01-08 22:07:21 +0100

    Added wrapper around WifiServer

---
+++ ESPWifiControlledLegoCar/WifiServerWrapper.hpp
@@ -0,0 +1,29 @@
+#ifndef WIFI_SERVER_WRAPPER
+#define WIFI_SERVER_WRAPPER
+
+#include <ESP8266WiFi.h>
+
+class WifiServerWrapper
+{
+public:
+    /// Argument: ellapsed time since attempt at connection
+    using WatingForConnectionCallback = void(*)(unsigned long);
+
+    /// Argument: http client and http request header
+    using HandleHttpRequestCallback = void(*)(WiFiClient&, const String&);
+
+    WifiServerWrapper(uint16_t port);
+
+    bool Initialize(
+        const char* networkName,
+        const char* networkPass,
+        WatingForConnectionCallback waitForConnection = nullptr,
+        unsigned long networkConnectionTimeout = 5000);
+
+    void HandleConnection(HandleHttpRequestCallback handleHttpRequest, unsigned long connectionTimeout = 2000);
+
+private:
+    WiFiServer m_Server;
+};
+
+#endif // WIFI_SERVER_WRAPPER
